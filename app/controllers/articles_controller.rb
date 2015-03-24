28948846a69d9b54d621c690e62e14894c21cf1e
Sciborowska Basia <the-tjorven@hotmail.com>
2015-03-24 14:31:29 +0000

    trzeciezajecia

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
-  
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  before_filter :authenticate_user!, except:[:index]
+  load_and_authorize_resource
   
   def index
     @articles = Article.all
