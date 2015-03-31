b85dfc971d56bb94cf66eb02993a9de36a03dd87
Sciborowska Basia <the-tjorven@hotmail.com>
2015-03-31 12:39:34 +0000

    addbootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_filter :authenticate_user!, except:[:index]
+  #before_filter :authenticate_user!, except:[:index]
   load_and_authorize_resource
   
   def index
