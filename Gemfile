6f337e6044f975884153e14a68d5ee40d86107ef
Sciborowska Basia <the-tjorven@hotmail.com>
2015-04-14 13:07:22 +0000

    factoriesusers

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ group :development, :test do
   gem 'rspec'
   gem 'rspec-rails', '~> 3.0'
   gem 'shoulda-matchers'
+  gem 'factory_girl_rails'
   
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 describe User do
   it{should validate_presence_of :first_name}
   before :each do
-     @user = User.new(first_name: 'Johnny', last_name: 'Smith')
+     @user = create(:user) 
   end
   describe 'full name' do
     it 'returns user full name' do
-      expect(@user.full_name).to eq 'Johnny Smith'
+      expect(@user.full_name).to eq @user.first_name + ' ' + @user.last_name
     end
   end
   describe 'initials' do
