a50d60b43b5e34b3fae1d8d323b3bb449c46a230
Sciborowska Basia <the-tjorven@hotmail.com>
2015-04-21 12:38:56 +0000

    newarticlescontroller

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
-before :each do
-  article = create(:article)
-end
 
 describe ArticlesController do
+  before :each do
+    @article = create(:article)
+  end
+  
   describe 'index' do
     it 'return articles' do
       get :index
@@ -15,11 +16,22 @@ describe ArticlesController do
   
   describe 'show' do
     it 'return a article' do
-      get :show, id: article.id
+      get :show, id: @article.id
       
       expect(response).to have_http_status(:success)
-      expect(assigns(:articles)).not_to eq nil
-      expect(assigns(:articles).title).to eq @article.title
+      expect(assigns(:article)).not_to eq nil
+      expect(assigns(:article).title).to eq @article.title
+    end
+  end
+  
+  describe 'new' do
+    it 'return a new article' do
+      @request.env['devise.mapping'] = Devise.mappings[:admin]
+      sign_in FactoryGirl.create(:admin)
+      get :new
+      
+      expect(response).to have_http_status(:success)
+      expect(assigns(:article)).not_to eq nil
     end
   end
 end
