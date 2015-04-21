bbff1b38512d2facf33a9d975308f8579d75b82c
Sciborowska Basia <the-tjorven@hotmail.com>
2015-04-21 12:20:37 +0000

    showarticlescontroller

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
+before :each do
+  article = create(:article)
+end
 
 describe ArticlesController do
   describe 'index' do
     it 'return articles' do
-      article = create(:article)
       get :index
       
       expect(response).to have_http_status(:success)
       expect(assigns(:articles).length).to eq 1
+    end
+  end
+  
+  describe 'show' do
+    it 'return a article' do
+      get :show, id: article.id
+      
+      expect(response).to have_http_status(:success)
+      expect(assigns(:articles)).not_to eq nil
+      expect(assigns(:articles).title).to eq @article.title
     end
   end
 end
